c4ef11844b006a12d8b1a95e2d024049fae7b869
Miguel de la Cruz <mgdelacroix@gmail.com>
2015-07-11 00:25:39 +0200

    Add jade templates and babel processing

---
+++ app/js/kaka.js
@@ -0,0 +1,3 @@
+let a = () => {
+  console.log("Hello World");
+};
--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,35 @@
 var gulp = require("gulp"),
     browserSync = require("browser-sync").create(),
-    paths = {
-      dist: "dist"
-    };
+    babel = require("gulp-babel"),
+    jade = require("gulp-jade");
 
 gulp.task("browser-sync", function() {
   browserSync.init({
     server: {
-      baseDir: paths.dist
-    }
+      baseDir: "dist"
+    },
+    notify: false,
+    open: false
   });
 });
 
-gulp.task("watch", ["browser-sync"], function() {
+gulp.task("templates", function() {
+  return gulp.src("app/templates/**/*.jade")
+    .pipe(jade())
+    .pipe(gulp.dest("dist"))
+  ;
+});
 
+gulp.task("js", function() {
+  return gulp.src("app/js/**/*.js")
+    .pipe(babel())
+    .pipe(gulp.dest("dist/assets/js"))
+  ;
+});
+
+gulp.task("dist", ["templates", "js"]);
+
+gulp.task("watch", ["dist", "browser-sync"], function() {
+  gulp.watch("app/templates/**/*.jade", ["templates", browserSync.reload]);
+  gulp.watch("app/js/**/*.js", ["js", browserSync.reload]);
 });
