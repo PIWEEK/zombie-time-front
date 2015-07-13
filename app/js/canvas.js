b76968e652d253529d1ab044b8d0577444c96bf5
Miguel de la Cruz <mgdelacroix@gmail.com>
2015-07-13 12:25:28 +0200

    Canvas autoresize on window size change

--- app/js/canvas.js
+++ app/js/canvas.js
@@ -1,15 +1,20 @@
 class Canvas {
   constructor() {
-    let el = document.createElement("canvas"),
-        viewportSize = utils.getViewportSize();
-
+    let el = document.createElement("canvas");
     el.id = "mainCanvas";
-    el.width = viewportSize.width;
-    el.height = viewportSize.height;
     document.querySelector('#content').appendChild(el);
-
     this.el = el;
     this.context = this.el.getContext("2d");
+
+    this.resize();
+  }
+
+  resize() {
+    let viewportSize = utils.getViewportSize(),
+        el = this.el;
+
+    el.width = viewportSize.width;
+    el.height = viewportSize.height;
   }
 
   sayHello() {
--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,5 @@
 let canvas = new Canvas();
 
+window.onresize = () => canvas.resize();
+
 canvas.sayHello();
