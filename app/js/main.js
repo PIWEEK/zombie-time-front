a0cfe090aeddf5598117f8051d8cb36e44282b47
Miguel de la Cruz <mgdelacroix@gmail.com>
2015-07-13 18:12:07 +0200

    Help dialog

--- app/js/main.js
+++ app/js/main.js
@@ -2,13 +2,15 @@ let canvas = new Canvas();
 
 window.onresize = () => canvas.resize();
 
-
 let test = () => {
-  let imageSrc = "/assets/imgs/cat.gif";
+  let imageSrc = "/assets/imgs/tile.png";
   canvas.addObject("tokens", "cat", imageSrc, 0, 0);
-  canvas.addObject("tokens", "cat2", imageSrc, 100, 100);
 }();
 
+
+/**********************************************
+ * Keybindings
+ **********************************************/
 Mousetrap.bind('i', function() {
   canvas.zoomIn(0.4);
 });
@@ -21,6 +23,18 @@ Mousetrap.bind('r', function() {
   canvas.zoomReset();
 });
 
+Mousetrap.bind('?', function() {
+  $('#help').show();
+}, 'keydown');
+
+Mousetrap.bind('?', function() {
+  $('#help').hide();
+}, 'keyup');
+
+
+/**********************************************
+ * Mouse scroll
+ **********************************************/
 canvas.el.onwheel = function(e) {
   if (e.deltaY >= 0) {
     canvas.zoomOut();
@@ -28,3 +42,36 @@ canvas.el.onwheel = function(e) {
     canvas.zoomIn();
   }
 };
+
+
+/**********************************************
+ * Drag and Drop
+ **********************************************/
+canvas.el.addEventListener("mousedown", function(e) {
+  canvas.drag = {
+    x: e.x,
+    y: e.y
+  };
+  console.log(`DRAG START { x: ${e.x}, y: ${e.y} }`);
+});
+
+canvas.el.addEventListener("mousemove", function(e) {
+  if (canvas.drag) {
+    let deltaX = e.x - canvas.drag.x,
+        deltaY = e.y - canvas.drag.y;
+
+    canvas.drag.x = e.x;
+    canvas.drag.y = e.y;
+
+    console.log(`MOVING { x: ${deltaX}, y: ${deltaY} }`);
+    canvas.ctx.translate(deltaX, deltaY);
+    console.log(`> offsetX ${e.offsetX}`);
+    console.log(`> offsetY ${e.offsetY}`);
+    canvas.redraw();
+  }
+});
+
+canvas.el.addEventListener("mouseup", function(e) {
+  canvas.drag = undefined;
+  console.log(`DRAG END { x: ${e.x}, y: ${e.y} }`);
+});
