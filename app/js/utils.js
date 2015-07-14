9b46eeea2055ed0886c6885ba640bcce3a74ccd5
Miguel de la Cruz <mgdelacroix@gmail.com>
2015-07-14 16:11:00 +0200

    Add loadImage to the utils functions

--- app/js/utils.js
+++ app/js/utils.js
@@ -1,5 +1,14 @@
+/*global Image */
+
 let utils = {
   getViewportSize: () => {
     return { width: window.innerWidth, height: window.innerHeight };
+  },
+  loadImage: (imageUrl) => {
+    return new Promise((resolve, reject) => {
+      let image = new Image();
+      image.onload = () => resolve(image);
+      image.src = imageUrl;
+    });
   }
 };
