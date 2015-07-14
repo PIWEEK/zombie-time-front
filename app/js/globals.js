d8bf0f789e744fdbfdbe6ed80fbcbbbee0a67101
Miguel de la Cruz <mgdelacroix@gmail.com>
2015-07-14 12:54:29 +0200

    Add empty classes to the game

--- app/js/globals.js
+++ app/js/globals.js
@@ -1,6 +1,9 @@
+/*global Http */
+
 const tileWidth = 256,
       tileHeight = 256,
       maxTilesWhenZoomIn = 4,
-      defaultZoomIncrement = 0.07;
+      defaultZoomIncrement = 0.07,
+      http = new Http();
 
 let app = {};
