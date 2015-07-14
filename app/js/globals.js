2b15f67a3ea837ee7262b1481d7c94162fcb2950
Miguel de la Cruz <mgdelacroix@gmail.com>
2015-07-14 17:12:11 +0200

    Add one more row to the default map size

--- app/js/globals.js
+++ app/js/globals.js
@@ -4,7 +4,7 @@ const conf = {
   tileWidth: 256,
   tileHeight: 256,
   spriteSizeX: 16,
-  spriteSizeY: 21,
+  spriteSizeY: 22,
   maxTilesWhenZoomIn: 4,
   defaultZoomIncrement: 0.07,
   serverUrl: "http://localhost:3000"
